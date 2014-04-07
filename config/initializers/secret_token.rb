42d1d12320d6c4bbb47328485c39ba812ee67922
matthova <matthova87@gmail.com>
2014-04-07 15:45:27 -0500

    starting over

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MyStudioSimplified::Application.config.secret_token = '20c5d8cae0a7b3e8c6f4921dd0db8f394fe31efa12a1b130f6c041eab993ae747efd65ef945b3aa8d08d8438407a229d6f6b62d01178a4d9efc4d10cdb7ef51f'
+MyStudioSimplified::Application.config.secret_token = '76125b04bc801f8f07b739090a1b50cefd7d345d75b9e2a2394ba89e61185c16ec63f6f7f618837a33fb4a4fb258d5dde5adfd7908bcc2998ffbd365a685b9f4'
