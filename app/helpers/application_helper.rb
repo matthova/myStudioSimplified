8f74bda2800c30d4160a16f10368bb7443d89dcb
matthova <matthova87@gmail.com>
2014-04-14 16:10:30 -0500

    less css more dry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
     end
   end 
 
+  def login_status(current_user)
+    if current_user
+      render :partial => "login_status", locals: { current_user: current_user }
+    end
+  end
+
 end
